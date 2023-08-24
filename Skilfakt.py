8f6407d5f0cf50f1a4d7f4085d61769cd4478d62
Olga-Val <kozachenko.o88@gmail.com>
2023-08-24 11:03:21 +0300

    python_2

--- Skilfakt.py
+++ Skilfakt.py
@@ -1,13 +1,30 @@
-zarplata={"БТК":250, "ММФ":230, "ЖЭУ":150 }
-chasi=int((input("Введите колличество рабочих часов:")))
-itogo=[]
-itogo.append(int(zarplata["БТК"]*chasi))
-itogo.append(int(zarplata["ММФ"]*chasi))
-itogo.append(int(zarplata["ЖЭУ"]*chasi))
-print("Итоговая зарплата без вычета налога:",itogo )
-itogo=float(input("Введите оклад без вычета налога:"))
-procent=float(input("Введите ставку подаходного налога:"))
-nalog=itogo*procent/100
-summa=itogo-nalog
-print("Итоговая зарплата после вычета налога:",summa)
+sumbil=0
+sumbil_1=0
+sumbil_2=0
+colbil=int(input("Введите колличество билетов, которое хотите приобрести:"))
+for age in range(colbil):
+    age=int(input("Введите возраст:"))
+    if age < 18:
+        sumbil+=0
+        print("Стоимость билета:",sumbil)
+    elif age >= 18 and age <= 25:
+        sumbil_1+=990
+        print("Стоимость билета:",sumbil_1)
+    elif age >25:
+        sumbil_2+=1390
+        print("Стоимость билета:",sumbil_2)
+sumbil_3=int(sumbil+sumbil_1+sumbil_2)
+print("Общая стоимость билетов:",sumbil_3)
+if colbil > 3:
+ sumbil_3 -= sumbil_3 / 100 * 10
+ print("Полная стоимость заказа со скидкой:", sumbil_3)
+
+
+
+
+
+
+
+
+
 
