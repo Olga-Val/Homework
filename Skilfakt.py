352fdf668f4c32f33049ad01f5efa8be31b72cfb
Olga-Val <kozachenko.o88@gmail.com>
2023-10-22 14:28:59 +0300

    python_2

--- Skilfakt.py
+++ Skilfakt.py
@@ -1,23 +1,14 @@
-sumbil=0
-sumbil_1=0
-sumbil_2=0
-colbil=int(input("Введите колличество билетов, которое хотите приобрести:"))
-for age in range(colbil):
-    age=int(input("Введите возраст:"))
-    if age < 18:
-        sumbil+=0
-        print("Стоимость билета:",sumbil)
-    elif age >= 18 and age <= 25:
-        sumbil_1+=990
-        print("Стоимость билета:",sumbil_1)
-    elif age >25:
-        sumbil_2+=1390
-        print("Стоимость билета:",sumbil_2)
-sumbil_3=int(sumbil+sumbil_1+sumbil_2)
-print("Общая стоимость билетов:",sumbil_3)
-if colbil > 3:
- sumbil_3 -= sumbil_3 / 100 * 10
- print("Полная стоимость заказа со скидкой:", sumbil_3)
+class Rectangle:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def get_area(self):
+        return self.a * self.b
+
+
+
+
+
 
 
 
